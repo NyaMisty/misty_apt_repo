60c55f18947e7c4a48519b676bd07c1301ebe0f5
NyaMisty <misty@misty.moe>
2023-11-10 14:17:30 +0800

    Support downloading multiple deb to support rootless

--- workflow_helper/pull_releases.sh
+++ workflow_helper/pull_releases.sh
@@ -7,12 +7,22 @@ REPO="$2"
 echo "Downloading release for $REPO"
 
 release_info=$(curl -s -u $CRED 'https://api.github.com/repos/'$REPO'/releases?per_page=1')
-release_fileurl=$(echo $release_info | jq -r ".[0].assets[0].url")
-release_filename=$(echo $release_info | jq -r ".[0].assets[0].name")
 
-if [[ "$release_file" == "null" ]]; then
-    echo "  No release found for $REPO"
-fi
+for i in {0..10}; do # max 10 artifacts
+    release_fileurl=$(echo "$release_info" | jq -r ".[0].assets[$i].url")
+    release_filename=$(echo "$release_info" | jq -r ".[0].assets[$i].name")
 
-echo "  Downloading release file $release_filename from $release_fileurl"
-curl -L -H 'Accept: application/octet-stream' -u $CRED -o "$release_filename" "$release_fileurl"
+    if [[ "$release_fileurl" == "null" ]]; then
+	if [ $i -eq 0 ]; then
+	    echo "  No release found for $REPO"
+	fi
+	break
+    fi
+
+    if ! [[ $release_filename == *.deb ]]; then
+        continue
+    fi
+
+    echo "  Downloading release file $release_filename from $release_fileurl"
+    curl -L -H 'Accept: application/octet-stream' -u $CRED -o "$release_filename" "$release_fileurl"
+done
